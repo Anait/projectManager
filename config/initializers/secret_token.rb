207eb6a24fe0aa89105813a85cdfb5cccd79e27b
Anait <ana_markosian@yahoo.com>
2012-11-28 13:13:55 -0800

    Create Rails app with postgreSQL db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = '8e68731aeeb276dc3aab5d3dded7cd4609abe94364928f5070c614f5e37e3892c75e3a6aa5add15f9c1323ab0b2ff9bf7c94213db4490ceed2866f2ee02dbc40'
